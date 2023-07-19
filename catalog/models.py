207d8dc76fe1e5105f1b23609fef16f6189f583e
svro <ichistrawberry2012@gmail.com>
2023-07-19 16:25:17 +0300

    work with models hw_20_1

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+
+    def __str__(self):
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=250, verbose_name='Наименование')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    picture = models.ImageField(**NULLABLE, upload_to='product/', verbose_name='Изображение')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
+    price = models.IntegerField(verbose_name='Цена')
+    data_create = models.DateTimeField(verbose_name='Дата создания')
+    data_edit = models.DateTimeField(verbose_name='Дата изменения')
+
+    def __str__(self):
+        return f'{self.name} {self.description} {self.price}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
+        ordering = ('name', 'data_create',)

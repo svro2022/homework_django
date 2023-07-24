d923a908b91293fd00d424df9c9d34ae15c2e942
svro <ichistrawberry2012@gmail.com>
2023-07-24 16:48:27 +0300

    for hw_20_2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from .views import home, contacts
+from .apps import CatalogConfig
+from .views import index, categories, category_products
 
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts')
+    # path('', home, name='home'),
+    # path('contacts/', contacts, name='contacts'),
+    path('', index, name='index'),
+    path('categories/', categories, name='categories'),
+    path('<int:pk>/products/', category_products, name='category_products'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,14 +1,47 @@
 from django.shortcuts import render
+from catalog.models import Category, Product
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'catalog/home.html')
+# def home(request):
+    # return render(request, 'catalog/home.html')
 
 
-def contacts(request):
-    if request.method == 'POST':
-        print(request.POST.get('name'))
-        print(request.POST.get('phone'))
-        print(request.POST.get('message'))
-    return render(request, 'catalog/contacts.html')
+# def contacts(request):
+    # if request.method == 'POST':
+       # print(request.POST.get('name'))
+       # print(request.POST.get('phone'))
+       # print(request.POST.get('message'))
+    # return render(request, 'catalog/contacts.html')
+
+
+def index(request):
+    '''контроллер главной страницы'''
+    context = {
+        'object_list': Category.objects.all()[:2],
+        'title': 'Онлайн магазин',
+        'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+def categories(request):
+    '''контроллер страницы всех категорий'''
+    context = {
+        'object_list': Category.objects.all(),
+        'title': 'Онлайн магазин',
+        'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
+
+    }
+    return render(request, 'catalog/categories.html', context)
+
+def category_products(request, pk):
+    '''контроллер страницы всех товаров'''
+    category_item = Category.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(category_id=pk),
+        'title': f'Товары из {category_item.name}',
+        'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.'
+
+    }
+    return render(request, 'catalog/products.html', context)

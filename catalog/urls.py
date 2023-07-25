0f6bb117a545ee1e917777c177200f6695ffdd83
svro <ichistrawberry2012@gmail.com>
2023-07-25 23:59:59 +0300

    for hw_20_2_edit_1

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
 from .apps import CatalogConfig
-from .views import index, categories, category_products
+from .views import index, categories, category_products, products, product_detailed
 
 app_name = CatalogConfig.name
 
@@ -11,4 +11,7 @@ urlpatterns = [
     path('', index, name='index'),
     path('categories/', categories, name='categories'),
     path('<int:pk>/products/', category_products, name='category_products'),
+    path('productsall/', products, name='products'),
+    path('<int:pk>/product/', product_detailed, name='product_detailed'),
+
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -18,7 +18,7 @@ from catalog.models import Category, Product
 def index(request):
     '''контроллер главной страницы'''
     context = {
-        'object_list': Category.objects.all()[:2],
+        'object_list': Product.objects.all(),
         'title': 'Онлайн магазин',
         'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
     }
@@ -36,7 +36,7 @@ def categories(request):
     return render(request, 'catalog/categories.html', context)
 
 def category_products(request, pk):
-    '''контроллер страницы всех товаров'''
+    '''контроллер страницы всех товаров с категориями'''
     category_item = Category.objects.get(pk=pk)
     context = {
         'object_list': Product.objects.filter(category_id=pk),
@@ -45,3 +45,32 @@ def category_products(request, pk):
 
     }
     return render(request, 'catalog/products.html', context)
+
+
+def products(request):
+    '''контроллер страницы всех товаров без категорий'''
+    products_list = Product.objects.all()
+    context = {
+        'object_list': products_list
+    }
+    return render(request, "catalog/products_all.html", context)
+
+
+def product_detailed(request, pk):
+    '''контроллер страницы одного товара'''
+    category_item = Product.objects.get(pk=pk)
+    context = {
+        'object_list': Product.objects.filter(pk=pk),
+        'title': f'Товар - {category_item.name}',
+        'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.',
+        'name_card': category_item.name,
+        'price': category_item.price,
+        'description': category_item.description,
+        'data_added': category_item.data_create,
+        'data_edit': category_item.data_edit
+
+    }
+    return render(request, "catalog/product_detail.html", context)
+
+
+

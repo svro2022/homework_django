dedcef803d65d3b27b027dd4a31e2d19cc099d91
svro <ichistrawberry2012@gmail.com>
2023-07-30 13:01:16 +0300

    hw_21_1_cbv

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,15 +1,20 @@
 from django.urls import path
 
 from .apps import CatalogConfig
-from .views import index, categories, category_products, products, product_detailed
+from .views import MainListView, ProductsListView, CategoriesListView, CategoryProductsListView, ProductDetailView
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('categories/', categories, name='categories'),
-    path('<int:pk>/products/', category_products, name='category_products'),
-    path('productsall/', products, name='products'),
-    path('<int:pk>/product/', product_detailed, name='product_detailed'),
+    #FBV path('', index, name='index'),
+    path('', MainListView.as_view(), name='index'),
+    #FBV path('categories/', categories, name='categories'),
+    path('categories/', CategoriesListView.as_view(), name='categories'),
+    #FBV path('<int:pk>/products/', category_products, name='category_products'),
+    path('<int:pk>/products/', CategoryProductsListView.as_view(), name='category_products'),
+    #FBV path('productsall/', products, name='products'),
+    path('productsall/', ProductsListView.as_view(), name='products'),
+    #FBV path('<int:pk>/product/', product_detailed, name='product_detailed'),
+    path('<int:pk>/product/', ProductDetailView.as_view(), name='product_detailed'),
 
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,65 +1,137 @@
 from django.shortcuts import render
 from catalog.models import Category, Product
+from django.views.generic import ListView, DetailView
 
 
 # Create your views here.
 
-def index(request):
-    '''контроллер главной страницы'''
-    context = {
-        'object_list': Product.objects.all(),
+# FBV подход
+#def index(request):
+    #'''контроллер главной страницы'''
+    #context = {
+       # 'object_list': Product.objects.all(),
+       # 'title': 'Онлайн магазин',
+       # 'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
+   # }
+   # return render(request, 'catalog/index.html', context)
+
+
+'''CBV подход'''
+class MainListView(ListView):
+    '''Контролер главной страницы. Отображает все товары без категорий'''
+    model = Product
+    template_name = 'catalog/index.html'
+    extra_context = {
+        'title': 'Онлайн магазин',
+        'title_text': 'Добро пожаловать! Выбираем нужный товар.'
+    }
+
+
+# FBV подход
+#def categories(request):
+    #'''контроллер страницы всех категорий'''
+    #context = {
+     #   'object_list': Category.objects.all(),
+      #  'title': 'Онлайн магазин',
+     #   'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
+
+    #}
+    #return render(request, 'catalog/categories.html', context)
+
+
+'''CBV подход'''
+class CategoriesListView(ListView):
+    '''Контроллер страницы Категории'''
+    model = Category
+    template_name = 'catalog/categories.html'
+    extra_context = {
         'title': 'Онлайн магазин',
         'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
     }
-    return render(request, 'catalog/index.html', context)
 
 
-def categories(request):
-    '''контроллер страницы всех категорий'''
-    context = {
-        'object_list': Category.objects.all(),
+# FBV подход
+# def category_products(request, pk):
+    #'''контроллер страницы всех товаров с категориями'''
+    #category_item = Category.objects.get(pk=pk)
+    #context = {
+    #    'object_list': Product.objects.filter(category_id=pk),
+    #    'title': f'Товары из {category_item.name}',
+    #    'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.'
+
+    #}
+    #return render(request, 'catalog/products.html', context)
+
+
+
+'''CBV подход'''
+class CategoryProductsListView(ListView):
+    '''Контроллер страницы товаров в конкретной Категории'''
+    model = Product
+    template_name = 'catalog/products.html'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(category_id=self.kwargs.get('pk'))
+        return queryset
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(*args, **kwargs)
+
+        category_item = Category.objects.get(pk=self.kwargs.get('pk'))
+        context_data['category_pk'] = category_item.pk,
+        context_data['title'] = f'Товары из {category_item.name}',
+        context_data['title_text'] = f'Добро пожаловать! Выбираем нужный Вам товар.'
+
+        return context_data
+
+
+
+# FBV подход
+# def products(request):
+   # '''контроллер страницы всех товаров без категорий'''
+   # products_list = Product.objects.all()
+   # context = {
+    #    'object_list': products_list
+   # }
+   # return render(request, "catalog/products_all.html", context)
+
+
+'''CBV подход'''
+class ProductsListView(ListView):
+    '''Контролер страницы Товары. Отображает все товары без категорий'''
+    model = Product
+    template_name = 'catalog/products_all.html'
+    extra_context = {
         'title': 'Онлайн магазин',
-        'title_text': 'Добро пожаловать! Выбираем нужную Вам категорию товаров.'
-
+        'title_text': 'Добро пожаловать! Ознакомьтесь с товарами.'
     }
-    return render(request, 'catalog/categories.html', context)
 
-def category_products(request, pk):
-    '''контроллер страницы всех товаров с категориями'''
-    category_item = Category.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(category_id=pk),
-        'title': f'Товары из {category_item.name}',
-        'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.'
 
+# FBV подход
+# def product_detailed(request, pk):
+    #'''контроллер страницы одного товара'''
+    #category_item = Product.objects.get(pk=pk)
+    #context = {
+    #    'object_list': Product.objects.filter(pk=pk),
+    #    'title': f'Товар - {category_item.name}',
+    #    'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.',
+    #    'name_card': category_item.name,
+    #    'price': category_item.price,
+    #    'description': category_item.description,
+    #    'data_added': category_item.data_create,
+    #    'data_edit': category_item.data_edit
+
+    #}
+    #return render(request, "catalog/product_detail.html", context)
+
+
+'''CBV подход'''
+class ProductDetailView(DetailView):
+    '''Контроллер страницы одного товара'''
+    model = Product
+    template_name = 'catalog/product_detail.html'
+    extra_context = {
+        'title': 'Онлайн магазин',
+        'title_text': 'Добро пожаловать! Ознакомьтесь с товаром.'
     }
-    return render(request, 'catalog/products.html', context)
-
-
-def products(request):
-    '''контроллер страницы всех товаров без категорий'''
-    products_list = Product.objects.all()
-    context = {
-        'object_list': products_list
-    }
-    return render(request, "catalog/products_all.html", context)
-
-
-def product_detailed(request, pk):
-    '''контроллер страницы одного товара'''
-    category_item = Product.objects.get(pk=pk)
-    context = {
-        'object_list': Product.objects.filter(pk=pk),
-        'title': f'Товар - {category_item.name}',
-        'title_text': 'Добро пожаловать! Выбираем нужный Вам товар.',
-        'name_card': category_item.name,
-        'price': category_item.price,
-        'description': category_item.description,
-        'data_added': category_item.data_create,
-        'data_edit': category_item.data_edit
-
-    }
-    return render(request, "catalog/product_detail.html", context)
-
-
-
